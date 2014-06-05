f1b658ba32b7b36e6c2d8e69e15ea2ea2afdadaf
Hitesh <hitesh.mehta@paytm.com>
2014-06-05 11:40:55 +0530

    Added csv attachment support

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Table = require('cli-table');
 var util = require('util');
 
 function QTable(options) {
-  if (options) {
+  if (options && options.mail) {
     this.mail = options.mail;
     this.title = options.title;
   }
@@ -25,102 +25,110 @@ var mailflag = true;
 if (tty.isatty(1) == false) {
   QTable.prototype.toString = function() {
     var options = this.options;
-    var str = [];
-    var colsum = options.count;
-    var length = this.length;
-    var wfirst,sum;
-    
-    if (this.mail && mailflag) {
-      str = [ 
-        'Subject: ' + this.mail.subject,
-        'MIME-Version: 1.0',
-        'From: ' + this.mail.from,
-        'To:' + this.mail.to,
-        'Content-Type: text/html',
-        'Content-Disposition: inline',
-        ''
-      ];
-      mailflag= false;
-    }
+      var currDate = Date.now();
+      var str = [], header = [], superHeader = [], boundary = "\n--" + currDate + "\n";
+      if (this.mail && mailflag) {
 
+    header = [ 
+          'Subject: ' + this.mail.subject,
+          'MIME-Version: 1.0',
+          'From: ' + this.mail.from,
+          'To:' + this.mail.to,
+          'Content-Type: multipart/mixed;boundary=' + currDate,
+          ''
+        ];
+        mailflag= false;
+      }
 
-    // if width is specified for all columns, normalize to 100%
-    if (options.cols && options.cols.length == options.head.length) {
-      sum = options.cols.reduce(function(a,b) { return a+b; });
-      options.cols = options.cols.map(function(c) { return ((c*100)/sum)|0; });
-    }
+      if (this.title) {
+        str.push('<h2> ' + this.title + '</h2>');
+      }
 
-    str.push('<table cellpadding="0" cellspacing="0" width="100%" style="border: 1px solid;">');
+      str.push('<table cellpadding="0" cellspacing="0" width="100%" style="border: 1px solid;">');
 
-    if (this.title) {
-      str.push('<caption> ' + this.title + '</caption>');
-    }
-    wfirst = options.cols?(options.cols[0]|0):15;
+      str = str.join('\n');
 
-    str = str.join('\n');
-
-    if (options.head && options.head.length) {
-      str += [
-        '<thead>',
-        '<th style="padding:10px;" width="' + wfirst + '%">',
-        options.head.join('</th><th style="padding:10px;background-color:#eeeeee;">'),
-        '</th>',
-        '</thead>'
-      ].join('\n');
-    }
-
-    this.forEach(function(x,i) {
-      // x is a row of the table, k is the first element
-      var k,v,sum,wtotal = wfirst;
-      // cross tables
-      if (!util.isArray(x)) {
-        k = Object.keys(x)[0];
-        v = x[k];
-        if (util.isArray(v)) {
-          v = v.join('</td><td>');
+      if (options.head && options.head.length) {
+        str += [
+          '<thead>',
+          '<th style="padding:10px;" width="15%">',
+          options.head.join('</th><th style="padding:10px;background-color:#eeeeee;">'),
+          '</th>',
+          '</thead>'
+        ].join('\n');
+      }
+      var that = this;
+      this.forEach(function(x) {
+        // x is a row of the table, k is the first element
+        var k,v,sum;
+        // cross tables
+        if (!util.isArray(x)) {
+          k = Object.keys(x)[0];
+          v = x[k];
+          if (util.isArray(v)) {
+            v = v.join('</td><td>');
+          }
+          x = [v];
+        } else {
+          k = x[0] || '';
         }
-        x = [v];
-      } else {
-        k = x.shift() || '';
+        
+        if (options.percentwidth) {
+          sum = x.reduce(function(a,b) { return parseInt(a,10) +parseInt(b,10); });
+          v = x.map(function(k) { return (parseInt(k,10)*85/sum)|0; });
+          str += '</table><table width="100%" cellspacing="1" cellpadding="0">';
+        }
+
+        str += '<tr>' + 
+               '<td style="padding:10px;background-color:#eeeeee;" width="15%">' + k + '</td>' + 
+                x.map(function(k,idx) { 
+                  if(idx!=0){
+                    var width='',color='';
+                    if (options.percentwidth) {
+                      width = 'width="' + v[idx] + '%"';
+                      color = 'color:#ffffff;background-color:' + getbgcolor(idx) +';';
+                    }
+                    return '<td ' + width + ' style="padding:10px;' + color + '">' + k + '</td>'; 
+                  }
+                }).join('') + 
+                '</tr>\n';
+
+        if (options.percentwidth) {
+          str += '</table>';
+        }
+      });
+      str += (options.percentwidth?'':'</table>') + '<br/>\n';
+
+      var csv = "\n\n";
+      if(this.mail.attachment){
+       str += boundary;
+       str += [ 
+        'Content-Type: text/csv; name='+ currDate +'.csv',
+        'Content-Disposition: attachment; filename='+ currDate +'.csv',
+      ].join('\n');
+      if (options.head && options.head.length) {
+        csv += [
+            options.head.join(','),
+          ].join('\n');
       }
-
-      if (options.stacked) {
-        sum = x.reduce(function(a,b) { return parseInt(a,10) +parseInt(b,10); },0);
-        v = x.map(function(k) { return (parseInt(k,10)*(100-wtotal)/sum)|0; });
-        str += '</table><table style="table-layout:fixed;" width="100%" cellspacing="1" cellpadding="0">';
-      } else if (options.bar) {
-        v = x.map(function(k) { var x = ((parseInt(k,10)*(100-wtotal))/colsum)|0; return x; });
-        str += '</table><table width="100%" cellspacing="1" cellpadding="0" style="font-size:.8em;">';
+      csv += '\n';
+      that.forEach(function(x) {
+        // x is a row of the table, k is the first element
+        var k,v,sum;
+        
+        csv += x.map(function(k) { 
+                  return k; 
+                }).join(',');
+        csv += '\n';
+      });
       }
+      var subHeader = 'Content-Type: text/html\n\n';
+      header = header.join('\n') ;
+      str = header + boundary + subHeader + str;
+      str += csv  + boundary;
+      return str;
+    };
+  }
 
-      str += '<tr>' + 
-             '<td style="padding:10px;background-color:#eeeeee;" width="' + wtotal + '%">' + k + '</td>' + 
-              x.map(function(k,idx) { 
-                var width; 
-                var color='';
+  module.exports = QTable;
 
-                if (options.stacked || options.bar) {
-                  width = v[idx] || 1;
-                  wtotal += width;
-                  width = 'width="' + width + '%"';
-                  color = 'color:#ffffff;background-color:' + getbgcolor(idx) +';';
-                }
-                return '<td ' + width + ' style="padding:10px;' + color + '">' + k + '</td>'; 
-              }).join('');
-
-
-      if (options.bar && wtotal < 100) {
-        str += '<td width="' + (100-wtotal) + '%" style="padding:10px;background-color:#ffffff"></td>';
-      }
-
-      str += '</tr>\n';
-
-      if ( (options.stacked || options.bar) && (i == length - 1)) {
-        str += '</table>';
-      }
-    });
-    return str + '</table><br/>';
-  };
-}
-
-module.exports = QTable;
